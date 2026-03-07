58cc8b51eba785462be1d30e2c88274a9a7089f5
BONHEUR-EDISON <bonheurluundoedison@gmail.com>
2026-03-07 02:54:10 +0100

    sure4

--- src/components/MapContent.tsx
+++ src/components/MapContent.tsx
@@ -1,11 +1,12 @@
-'use client'
+"use client"
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 import L from "leaflet"
 import "leaflet/dist/leaflet.css"
 
-// Fix Leaflet default icons for Next.js
-delete L.Icon.Default.prototype._getIconUrl
+// Fix icons
+delete (L.Icon.Default.prototype as any)._getIconUrl
+
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: "/leaflet/marker-icon-2x.png",
   iconUrl: "/leaflet/marker-icon.png",
@@ -22,9 +23,10 @@ export default function MapContent() {
   return (
     <MapContainer center={[-1.676, 29.233]} zoom={6} className="h-full w-full">
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
+        attribution='&copy; OpenStreetMap'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
       {locations.map((loc, i) => (
         <Marker key={i} position={[loc.lat, loc.lng]}>
           <Popup>{loc.label}</Popup>
